1d8912a81822df9294df9798c9bef8e11891bc7d
Neko <pppuss_in_boots@163.com>
2016-12-04 16:35:00 +0800

    能判断出出现频率最高的语言

--- app/src/main/java/com/neko/selectgithubuser/ui/MainActivity.java
+++ app/src/main/java/com/neko/selectgithubuser/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -18,8 +19,11 @@ import com.neko.selectgithubuser.presenter.IViewSuccess;
 import com.neko.selectgithubuser.presenter.PresenterSelectUser;
 import com.neko.selectgithubuser.ui.adapter.CommonAdapter;
 import com.neko.selectgithubuser.ui.adapter.UniversalViewHolder;
+import com.neko.selectgithubuser.utils.WorkFactory;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity {
     private PresenterSelectUser mPresenterSelectUser;
@@ -27,6 +31,7 @@ public class MainActivity extends Activity {
     private ListView mListView;
     private Button mDoSelect;
     private CommonAdapter<GithubUserInfo> mAdapter;
+    private String no1 = "";
     private ProgressDialog mDialog;
 
     @Override
@@ -44,19 +49,26 @@ public class MainActivity extends Activity {
         mListView = (ListView) findViewById(R.id.main_lv);
         mDoSelect = (Button) findViewById(R.id.doSelect);
         mPresenterSelectUser = new PresenterSelectUser(new IView() {
-            @Override
-            public void onFail(String msg) {
-
-            }
 
             @Override
             public void onStart(String s) {
+                if (mDialog == null) {
+                    mDialog = new ProgressDialog(getApplication());
+                }
+                mDialog.setMessage(s);
+                mDialog.show();
+            }
 
+            @Override
+            public void onFail(String msg) {
+                Toast.makeText(getApplication(), msg, Toast.LENGTH_SHORT).show();
+                mDialog.dismiss();
             }
 
             @Override
             public void onError(String s) {
-
+                Toast.makeText(getApplication(), s, Toast.LENGTH_SHORT).show();
+                mDialog.dismiss();
             }
         });
     }
@@ -70,6 +82,8 @@ public class MainActivity extends Activity {
                     @Override
                     public void onSuccess(List<GithubUserInfo> githubUser) {
                         mAdapter.updateData(githubUser);
+                        statistics();
+                        mDialog.dismiss();
                     }
                 });
             }
@@ -81,13 +95,48 @@ public class MainActivity extends Activity {
             @Override
             protected void setViewData(GithubUserInfo item, final UniversalViewHolder holder) {
                 ImageView imageView = holder.getView(R.id.user_avatar_im_v);
-                holder.setViewText(R.id.user_name_tx_v, item.getName()).
-                        setViewText(R.id.user_favorLanguage_tx_v, String.format("%s%s", "偏爱语言：", item.getFavorLanguage()));
+                holder.setViewText(R.id.user_name_tx_v, item.getName());
+                if (item.getFavorLanguage() != null) {
+                    holder.setViewText(R.id.user_favorLanguage_tx_v, String.format("%s%s", "偏爱语言：", item.getFavorLanguage()));
+                } else {
+                    holder.setViewText(R.id.user_favorLanguage_tx_v, "该用户无偏爱语言");
+                }
                 String avatarUrl = item.getAvatarUrl();
                 Glide.with(getApplication()).load(avatarUrl).diskCacheStrategy(DiskCacheStrategy.RESULT)
                         .placeholder(R.mipmap.ic_launcher).into(imageView);
             }
         };
         mListView.setAdapter(mAdapter);
+    }
+
+    private void statistics() {
+        final Map<String, Integer> map = new HashMap<>();
+        WorkFactory.INSTANCE.mService.submit(new Runnable() {
+            @Override
+            public void run() {
+                List<GithubUserInfo> list = mAdapter.getAdapterData();
+                for (GithubUserInfo info : list
+                        ) {
+                    Integer i = map.get(info.getFavorLanguage());
+                    map.put(info.getFavorLanguage(), (i == null) ? 1 : i + 1);
+                }
+                int next, before = 0;
+
+                for (String key : map.keySet()
+                        ) {
+                    next = map.get(key);
+                    if (next > before) {
+                        no1 = key;
+                        before = next;
+                    }
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplication(), "这些用户中出现频率最高的语言是：" + no1, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
